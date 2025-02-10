4cbd619ece37b9b8e423c036d279074a81dfac03
Norbert Balaz <n.balaz@outlook.com>
2025-02-10 13:07:45 +0100

    feat: add basic hero section with introduction text

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,16 @@
+const Hero = () => {
+    return (
+        <section className="min-h-screen w-full flex flex-col relative">
+            <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+                <p className="sm:text-3xl text-2xl text-white font-medium text-center font-generalsans">Hi, ich bin Norbert <span className="waving-hand">👋</span></p>
+                <p className="hero_tag text-gray_gradient">Ich bin ein Full Stack Entwickler!</p>
+            </div>
+
+            <div className="w-full h-full absolute inset-0">
+
+            </div>
+        </section>
+    )
+}
+
+export default Hero
