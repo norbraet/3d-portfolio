585d689147f53b47419a0996b91a6d9d8d4d9bc6
Norbert Balaz <n.balaz@outlook.com>
2025-03-10 11:22:22 +0100

    fix: fixes camera position

--- src/components/HeroCamera.jsx
+++ src/components/HeroCamera.jsx
@@ -6,7 +6,7 @@ const HeroCamera = ({ children, isMobile }) => {
     const groupRef = useRef()
     
     useFrame((state, delta) => {
-        easing.damp3(state.camera.position, [0, 0, 20], 0.25, delta)
+        easing.damp3(state.camera.position, [0, 0, 30], 0.25, delta)
 
         if (!isMobile) {
             easing.dampE(groupRef.current.rotation, [-state.pointer.y / 3, -state.pointer.x / 5, 0], 0.25, delta)
