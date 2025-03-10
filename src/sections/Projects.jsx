48634b3b260b12c4a6d7c582a617d426921d525b
Norbert Balaz <n.balaz@outlook.com>
2025-03-10 14:36:56 +0100

    feat: add demo computer to display project specific video

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,5 +1,9 @@
-import React, { useState } from 'react'
+import React, { useState, Suspense} from 'react'
 import { myProjects } from '../constants'
+import { Canvas } from '@react-three/fiber'
+import { Center, OrbitControls } from '@react-three/drei'
+import CanvasLoader from '../components/CanvasLoader'
+import DemoComputer from '../components/DemoComputer'
 
 const Projects = () => {
     const [selectedProjectIndex, setSelectedProjectIndex] = useState(0)
@@ -52,6 +56,20 @@ const Projects = () => {
                                 <img src='/assets/right-arrow.png' alt='right arrow' />
                             </button>
                         </div>
+                    </div>
+                    <div className='border border-black-300 bg-black-200 rounded-lg h-96 md:h-full'>
+                        <Canvas>
+                            <ambientLight intensity={Math.PI} />
+                            <directionalLight position={[10, 10, 5]} />
+                            <Center>
+                                <Suspense fallback={<CanvasLoader />}>
+                                    <group scale={2} position={[0, -3, 0]} rotation={[0, -0.1, 0]}>
+                                        <DemoComputer texture={currentProject.texture}/>
+                                    </group>
+                                </Suspense>
+                            </Center>
+                            <OrbitControls maxPolarAngle={Math.PI / 2} enableZoom={false} />
+                        </Canvas>
                     </div>
                 </div>
             </div>
