f8eb94c1d76183892c0ff923e07defa882be5e58
Norbert Balaz <n.balaz@outlook.com>
2025-03-11 23:04:20 +0100

    feat: add interactive 3d model to the experience section

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -1,8 +1,14 @@
 import { Canvas } from '@react-three/fiber'
-import React from 'react'
+import React, { Suspense } from 'react'
 import { workExperiences } from '../constants'
+import { OrbitControls } from '@react-three/drei'
+import CanvasLoader from '../components/CanvasLoader'
+import Developer from '../components/Developer'
+import { useState } from 'react'
 
 const Experience = () => {
+
+    const [animationName, setAnimationName] = useState('idle')
     return (
         <section className='c-space my-20'>
             <div className='w-full text-white-600'>
@@ -10,14 +16,26 @@ const Experience = () => {
                 <div className='work-container'>
                     <div className='work-canvas'>
                         <Canvas>
-
+                            <ambientLight intensity={1} />
+                            <spotLight position={[10, 10, 10]} angle={0.15} penugbra={1} />
+                            <directionalLight position={[10, 10, 10]} intensity={3} />
+                            <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} />
+                            <Suspense fallback={<CanvasLoader />}>
+                                <Developer position-y={-3} scale={3} animationName={animationName} />
+                            </Suspense>
                         </Canvas>
                     </div>
 
                     <div className='work-content'>
                         <div className='sm:py-10 py-5 sm:px-5 px-2.5'>
                             {workExperiences.map(({id, name, pos, duration, title, animation, icon}) => (
-                                <div key={id} className='work-content_container group'>
+                                <div 
+                                    key={id} 
+                                    className='work-content_container group' 
+                                    onClick={() => setAnimationName(animation.toLowerCase())} 
+                                    onPointerOver={() => setAnimationName(animation.toLowerCase())} 
+                                    onPointerOut={() => setAnimationName('idle')}>
+
                                     <div className='flex flex-col h-full justify-start items-center py-2'>
                                         <div className='work-content_logo'>
                                             <img src={icon} alt='Logo' className='w-full h-full' />
